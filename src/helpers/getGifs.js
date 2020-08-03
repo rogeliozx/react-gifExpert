a558f2bced85aef237972674dcd363754760f1c7
Rogeliozx <rogelioltz@gmail.com>
2020-08-03 10:11:54 -0700

    First commi

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGif = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+    category
+  )}&api_key=EhXXl3PSDdfPkcmISE8DNGRv7xhBlObs`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+  const gifs = await data.map((img) => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url,
+    };
+  });
+  return gifs;
+};
