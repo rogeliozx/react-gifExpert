af067c9ba97e7f3fbd9785b19b060aa73f4b0250
Rogeliozx <rogelioltz@gmail.com>
2020-08-03 10:14:18 -0700

    docs added

---
+++ docs/precache-manifest.814c246ffa1d93f86b4bf670dbcd9fea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69efa6b23fa5904c7e0345723ddcbdad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30810c7dd7f00bf119e4",
+    "url": "/static/css/main.57dfda5b.chunk.css"
+  },
+  {
+    "revision": "52505c43845cc6838325",
+    "url": "/static/js/2.1610b3db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1610b3db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30810c7dd7f00bf119e4",
+    "url": "/static/js/main.7df16549.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
